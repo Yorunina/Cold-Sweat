dd23a487a0b97da3363148870be45f9b20ab14d7
MikulDev <gamer3mc@gmail.com>
2024-10-30 23:27:54 -0400

    Use fluid tags instead of hardcoding for hearth/icebox/boiler fuel
    Took 18 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -18,6 +18,7 @@ import com.momosoftworks.coldsweat.core.init.BlockEntityInit;
 import com.momosoftworks.coldsweat.core.init.ParticleTypesInit;
 import com.momosoftworks.coldsweat.core.network.ColdSweatPacketHandler;
 import com.momosoftworks.coldsweat.core.network.message.HearthResetMessage;
+import com.momosoftworks.coldsweat.data.tag.ModFluidTags;
 import com.momosoftworks.coldsweat.util.ClientOnlyHelper;
 import com.momosoftworks.coldsweat.util.compat.CompatManager;
 import com.momosoftworks.coldsweat.util.math.CSMath;
@@ -1180,8 +1181,9 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         @Override
         public int fill(FluidStack fluidStack, FluidAction fluidAction)
         {
-            if (fluidStack.getFluid() == Fluids.WATER)
-            {   int amount = Math.min(fluidStack.getAmount(), this.getTankCapacity(0) - coldFuel.getAmount());
+            if (fluidStack.getFluid().is(ModFluidTags.COLD))
+            {
+                int amount = Math.min(fluidStack.getAmount(), this.getTankCapacity(0) - coldFuel.getAmount());
                 if (fluidAction.execute())
                 {
                     if (coldFuel.isEmpty())
@@ -1191,8 +1193,9 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
                 }
                 return amount;
             }
-            else if (fluidStack.getFluid() == Fluids.LAVA)
-            {   int amount = Math.min(fluidStack.getAmount(), this.getTankCapacity(1) - hotFuel.getAmount());
+            else if (fluidStack.getFluid().is(ModFluidTags.HOT))
+            {
+                int amount = Math.min(fluidStack.getAmount(), this.getTankCapacity(1) - hotFuel.getAmount());
                 if (fluidAction.execute())
                 {
                     if (hotFuel.isEmpty())
