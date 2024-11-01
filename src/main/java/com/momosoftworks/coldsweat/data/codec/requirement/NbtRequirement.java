4b380ab8e598774a09f1c7f6f6772d0a5d28c944
MikulDev <gamer3mc@gmail.com>
2024-11-01 15:36:19 -0400

    Simplify NBT range check code

--- src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/NbtRequirement.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/NbtRequirement.java
@@ -109,11 +109,7 @@ public record NbtRequirement(CompoundTag tag)
             try
             {
                 // Parse value ranges in to min and max values
-                String[] ranges = string.getAsString().split("-");
-                Double[] range = new Double[ranges.length];
-                for (int i = 0; i < ranges.length; i++)
-                {   range[i] = Double.parseDouble(ranges[i]);
-                }
+                Double[] range = Arrays.stream(string.getAsString().split("-")).map(Double::parseDouble).toArray(Double[]::new);
                 if (range.length == 2)
                 {   return CSMath.betweenInclusive(otherNumber.getAsDouble(), range[0], range[1]);
                 }
