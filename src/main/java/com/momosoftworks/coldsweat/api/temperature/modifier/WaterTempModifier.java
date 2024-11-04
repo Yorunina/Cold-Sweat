9466680603609590a3d6c0e9637b77f1a06cd249
MikulDev <gamer3mc@gmail.com>
2024-11-03 19:54:11 -0500

    Add "dirty" property to TempModifiers

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/WaterTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/WaterTempModifier.java
@@ -1,7 +1,6 @@
 package com.momosoftworks.coldsweat.api.temperature.modifier;
 
 import com.momosoftworks.coldsweat.api.util.Temperature;
-import com.momosoftworks.coldsweat.core.network.message.ParticleBatchMessage;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
@@ -12,6 +11,10 @@ import java.util.function.Function;
 
 public class WaterTempModifier extends TempModifier
 {
+    private static final double WATER_SOAK_SPEED = 0.05;
+    private static final double RAIN_SOAK_SPEED = 0.0125;
+    private static final double DRY_SPEED = 1/320d;
+
     public WaterTempModifier()
     {
         this(0.01);
@@ -37,20 +40,23 @@ public class WaterTempModifier extends TempModifier
     public Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
         double worldTemp = Temperature.get(entity, Temperature.Trait.WORLD);
-        double maxTemp = ConfigSettings.MAX_TEMP.get();
-        double minTemp = ConfigSettings.MIN_TEMP.get();
+        double midTemp = CSMath.average(ConfigSettings.MAX_TEMP.get(), ConfigSettings.MIN_TEMP.get());
 
         double strength = this.getNBT().getDouble("Strength");
-        double returnRate = Math.min(-0.0012, -0.0012 - (worldTemp / 640));
-        double addAmount = WorldHelper.isInWater(entity) ? 0.05 : WorldHelper.isRainingAt(entity.level(), entity.blockPosition()) ? 0.0125 : returnRate;
-        double maxStrength = CSMath.clamp(Math.abs(CSMath.average(maxTemp, minTemp) - worldTemp) / 2, 0.23d, 0.5d);
+        double addAmount = WorldHelper.isInWater(entity) ? WATER_SOAK_SPEED // In water
+                         : WorldHelper.isRainingAt(entity.level(), entity.blockPosition()) ? RAIN_SOAK_SPEED // In rain
+                         : Math.min(-DRY_SPEED, -DRY_SPEED - worldTemp * DRY_SPEED); // Drying off
+        double maxStrength = CSMath.clamp(Math.abs(midTemp - worldTemp) / 2, 0.23d, 0.5d);
 
         double newStrength = CSMath.clamp(strength + addAmount, 0d, maxStrength);
         this.getNBT().putDouble("Strength", newStrength);
+        if (strength != newStrength)
+        {   this.markDirty();
+        }
 
         // If the strength is 0, this TempModifier expires
         if (strength <= 0.0)
-        {   this.expires(this.getTicksExisted() - 1);
+        {   this.expires(0);
         }
 
         return temp ->
@@ -62,8 +68,7 @@ public class WaterTempModifier extends TempModifier
                     double randX = entity.getBbWidth() * (Math.random() - 0.5);
                     double randY = entity.getBbHeight() * Math.random();
                     double randZ = entity.getBbWidth() * (Math.random() - 0.5);
-                    new ParticleBatchMessage(2).addParticle(ParticleTypes.FALLING_WATER, entity.getX() + randX, entity.getY() + randY, entity.getZ() + randZ, 0, 0, 0)
-                                               .sendEntity(entity);
+                    entity.level().addParticle(ParticleTypes.FALLING_WATER, entity.getX() + randX, entity.getY() + randY, entity.getZ() + randZ, 0, 0, 0);
                 }
             }
             return temp - newStrength;
