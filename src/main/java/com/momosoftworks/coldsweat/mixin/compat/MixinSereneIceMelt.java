169c12e22dd4a1b7e4e14ea5934d2c257c19c3ea
MikulDev <gamer3mc@gmail.com>
2024-11-28 11:46:54 -0500

    Fix ice mixin crash at tickspeed 0 & make tickspeed affect melting

--- src/main/java/com/momosoftworks/coldsweat/mixin/compat/MixinSereneIceMelt.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/compat/MixinSereneIceMelt.java
@@ -2,14 +2,17 @@ package com.momosoftworks.coldsweat.mixin.compat;
 
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
+import glitchcore.event.TickEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import sereneseasons.season.RandomUpdateHandler;
@@ -20,7 +23,8 @@ public class MixinSereneIceMelt
     @Inject(method = "meltInChunk",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/IceBlock;melt(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"),
             locals = LocalCapture.CAPTURE_FAILHARD,
-            cancellable = true)
+            cancellable = true,
+            remap = false)
     private static void getBiomeTemperatureOverride(ChunkMap chunkManager, LevelChunk chunkIn, float meltChance, CallbackInfo ci,
                                                     //locals
                                                     ServerLevel level, ChunkPos chunkPos, int minX, int minZ, BlockPos topAirPos)
@@ -29,8 +33,22 @@ public class MixinSereneIceMelt
 
         BlockPos groundPos = topAirPos.below();
         if (WorldHelper.getWorldTemperatureAt(level, groundPos) < 0.15F)
-        {
-            ci.cancel();
+        {   ci.cancel();
         }
+    }
+
+    @ModifyVariable(method = "onWorldTick",
+                    at = @At(value = "STORE", ordinal = 0),
+                    remap = false)
+    private static int tickSpeedMeltRolls(int rolls, TickEvent.Level event)
+    {
+        if (!ConfigSettings.USE_CUSTOM_WATER_FREEZE_BEHAVIOR.get()) return rolls;
+
+        if (event.getLevel() instanceof ServerLevel level)
+        {
+            int tickSpeed = level.getGameRules().getInt(GameRules.RULE_RANDOMTICKING);
+            return rolls * tickSpeed / 3;
+        }
+        return rolls;
     }
 }
