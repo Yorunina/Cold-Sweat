b4d4e3f72c9546911a63de7f6cc71ead8eb5e035
MikulDev <gamer3mc@gmail.com>
2024-11-28 20:56:33 -0500

    Fix config menu toggle buttons not working properly

--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
@@ -35,9 +35,6 @@ public abstract class AbstractConfigPage extends Screen
     static int MOUSE_STILL_TIMER = 0;
     static int TOOLTIP_DELAY = 5;
 
-    public final String ON = CommonComponents.OPTION_ON.getString();
-    public final String OFF = CommonComponents.OPTION_OFF.getString();
-
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Pre event)
     {   MOUSE_STILL_TIMER++;
@@ -200,8 +197,8 @@ public abstract class AbstractConfigPage extends Screen
         // Make the button
         Button button = new ConfigButton(buttonX + xOffset, buttonY, buttonWidth, 20, label, button1 ->
         {
-            button1.setMessage(dynamicLabel.get());
             onClick.accept(button1);
+            button1.setMessage(dynamicLabel.get());
         })
         {
             @Override
@@ -657,8 +654,10 @@ public abstract class AbstractConfigPage extends Screen
         ConfigScreen.saveConfig();
     }
 
-    public MutableComponent getToggleButtonText(Component text, boolean on)
-    {   return Component.literal(text.getString() + ": " + (on ? ON : OFF));
+    public MutableComponent getToggleButtonText(MutableComponent text, boolean on)
+    {
+        return text.append(": ")
+                   .append(on ? CommonComponents.OPTION_ON : CommonComponents.OPTION_OFF);
     }
 
     public MutableComponent getSliderPercentageText(MutableComponent message, double value, double offAt)
--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/pages/ConfigPageOne.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/pages/ConfigPageOne.java
@@ -105,22 +105,22 @@ public class ConfigPageOne extends AbstractConfigPage
 
         // Misc. Temp Effects
         this.addButton("ice_resistance", Side.RIGHT,
-                () -> Component.translatable("cold_sweat.config.ice_resistance.name").append(": ").append(ConfigSettings.ICE_RESISTANCE_ENABLED.get() ? ON : OFF),
+                () -> getToggleButtonText(Component.translatable("cold_sweat.config.ice_resistance.name"), ConfigSettings.ICE_RESISTANCE_ENABLED.get()),
                 button -> ConfigSettings.ICE_RESISTANCE_ENABLED.set(!ConfigSettings.ICE_RESISTANCE_ENABLED.get()),
                 true, true, false, Component.translatable("cold_sweat.config.ice_resistance.desc"));
 
         this.addButton("fire_resistance", Side.RIGHT,
-                () -> Component.translatable("cold_sweat.config.fire_resistance.name").append(": ").append(ConfigSettings.FIRE_RESISTANCE_ENABLED.get() ? ON : OFF),
+                () -> getToggleButtonText(Component.translatable("cold_sweat.config.fire_resistance.name"), ConfigSettings.FIRE_RESISTANCE_ENABLED.get()),
                 button -> ConfigSettings.FIRE_RESISTANCE_ENABLED.set(!ConfigSettings.FIRE_RESISTANCE_ENABLED.get()),
                 true, true, false, Component.translatable("cold_sweat.config.fire_resistance.desc"));
 
         this.addButton("require_thermometer", Side.RIGHT,
-                () -> Component.translatable("cold_sweat.config.require_thermometer.name").append(": ").append(ConfigSettings.REQUIRE_THERMOMETER.get() ? ON : OFF),
+                () -> getToggleButtonText(Component.translatable("cold_sweat.config.require_thermometer.name"), ConfigSettings.REQUIRE_THERMOMETER.get()),
                 button -> ConfigSettings.REQUIRE_THERMOMETER.set(!ConfigSettings.REQUIRE_THERMOMETER.get()),
                 true, true, false, Component.translatable("cold_sweat.config.require_thermometer.desc"));
 
         this.addButton("use_peaceful", Side.RIGHT,
-                () -> Component.translatable("cold_sweat.config.use_peaceful.name").append(": ").append(ConfigSettings.USE_PEACEFUL_MODE.get() ? ON : OFF),
+                () -> getToggleButtonText(Component.translatable("cold_sweat.config.use_peaceful.name"), ConfigSettings.USE_PEACEFUL_MODE.get()),
                 button -> ConfigSettings.USE_PEACEFUL_MODE.set(!ConfigSettings.USE_PEACEFUL_MODE.get()),
                 true, true, false, Component.translatable("cold_sweat.config.use_peaceful.desc"));
     }
