8f196b8e8a87e261a0dfb180692d7416498d7e7d
MikulDev <gamer3mc@gmail.com>
2024-12-19 22:15:30 -0500

    Fix hearth not regularly checking for fuel

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -308,7 +308,6 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         }
         if (!this.shouldUseColdFuel && !this.shouldUseHotFuel && !this.paths.isEmpty())
         {   this.forceUpdate();
-            this.resetPaths();
         }
 
         // Reset if a nearby block has been updated
@@ -385,13 +384,14 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
                 }
 
                 // Drain fuel
-                this.tickDrainFuel();
+                if (!isClient)
+                {   this.tickDrainFuel();
+                }
             }
         }
 
         // Update fuel
-        if (!this.level.isClientSide && this.isFuelChanged()
-        || (wasUsingColdFuel != this.shouldUseColdFuel || wasUsingHotFuel != this.shouldUseHotFuel))
+        if (!this.level.isClientSide && (this.isFuelChanged() || wasUsingColdFuel != this.shouldUseColdFuel || wasUsingHotFuel != this.shouldUseHotFuel))
         {   this.updateFuelState();
         }
 
@@ -570,15 +570,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
     @Override
     public void setChanged()
     {
-        this.setChanged(true);
-    }
-
-    public void setChanged(boolean updateFuel)
-    {
-        if (updateFuel)
-        {   this.checkForFuel();
-        }
         super.setChanged();
+        this.checkForFuel();
     }
 
     public void checkForFuel()
@@ -620,8 +613,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
             {
                 int itemFuel = getItemFuel(fuelStack);
                 if (itemFuel != 0)
-                {
-                    this.storeFuel(fuelStack, itemFuel);
+                {   this.storeFuel(fuelStack, itemFuel);
                 }
             }
         }
@@ -629,7 +621,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
 
     protected boolean isFuelChanged()
     {
-        return Math.abs(this.getColdFuel() - lastColdFuel) >= this.getMaxFuel()/36 || Math.abs(this.getHotFuel() - lastHotFuel) >= this.getMaxFuel()/36;
+        return this.getColdFuel() != lastColdFuel || this.getHotFuel() != lastHotFuel;
     }
 
     protected void storeFuel(ItemStack stack, int amount)
@@ -663,6 +655,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
     {
         if (this.ticksExisted % 40 == 0)
         {   this.drainFuel();
+            this.checkForFuel();
         }
     }
 
@@ -907,8 +900,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
     public void updateFuelState()
     {
         if (level != null && !level.isClientSide)
-        {   this.setChanged(false);
-            WorldHelper.syncBlockEntityData(this);
+        {   WorldHelper.syncBlockEntityData(this);
             this.lastColdFuel = this.getColdFuel();
             this.lastHotFuel = this.getHotFuel();
         }
